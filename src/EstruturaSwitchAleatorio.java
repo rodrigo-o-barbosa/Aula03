c76f2a2d3bc9e2b02eb1d5496fc64ef88291fada
Rodrigo Barbosa <rodrigo.olv.barbosa@gmail.com>
2021-06-28 12:35:22 -0300

    Criador de sorteador para os elogios

---
+++ src/EstruturaSwitchAleatorio.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class EstruturaSwitchAleatorio {
+
+	public static void main(String[] args) {
+		/*Crie um programa que permita ao usuário escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio físico, 3 - Elogio pessoal
+		 * Caso digite qualquer outro número, deve ser exibida a mensagem "opção inválida"*/
+		Random gerador = new Random();
+		int opcao;
+		System.out.println("ELOGIADOR!");
+		opcao = gerador.nextInt(3);
+		System.out.println(opcao);
+		//Aqui precisamos de uma estrutura para validar a opção inserida.
+		switch(opcao)
+		{
+		case 0:
+			System.out.println("Os seus ifs são lindos demais!");
+			break;
+		case 1:
+			System.out.println("A sua densidade óssea é incrível!");
+			break;
+		case 2:
+			System.out.println("Você é uma pessoa sensacional!");
+			break;
+		default:
+			System.out.println("Opção inválida");
+		}
+
+	}
+
+}
