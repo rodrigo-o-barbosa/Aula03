9098e5c6085b0c6c6afb52489db196f865da4757
Rodrigo Barbosa <rodrigo.olv.barbosa@gmail.com>
2021-06-28 11:38:08 -0300

    Implementado o operador OU

--- src/OperadorOu.java
+++ src/OperadorOu.java
@@ -14,7 +14,14 @@ public class OperadorOu {
 		cupom = leitor.next();
 		
 		//Aqui é preciso criar um if que verifique as duas condições para que o desconto seja ou não aplicado
-		
+		if (totalCompra > 1000 || cupom.equalsIgnoreCase("desconto20"))
+		{
+			valorFinal = totalCompra * 0.8;
+		}
+		else
+		{
+			valorFinal = totalCompra;
+		}
 		System.out.println("O valor final é de R$" + valorFinal);
 
 	}
